79d339312397aa4c9a81f4eb7fd915a07b9d85a8
Shwanjeongdae <www.linheinhtet347@gamil.com>
2022-05-03 07:15:31 +0630

    Author Route

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const express = require('express');
 const expressEjsLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 // import cors from "cors";
-// import bodyParser from 'body-parser';
+const bodyParser = require('body-parser');
 const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
+
 const app = express();
     app.set('view engine', 'ejs')
     app.set("views", __dirname + "/views")
     app.set('layout', "layouts/layout")
     app.use(expressEjsLayouts);
     app.use(express.static("public"))
+    app.use(bodyParser.urlencoded({limit:"10mb", extended:false}))
 
     mongoose.connect(process.env.DATABASE_URL , {
         useNewUrlParser: true,
@@ -26,8 +29,8 @@ const app = express();
 
 
      app.use("/" , indexRouter)
+     app.use("/authors", authorRouter)
 // // routes
 // app.use(cors())
-// app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 3000)
